b5437ba43a479360c55803777a0fe07c99e9d7d9
Pavels Sokolovs <pavels.sokolovs@repharm.lv>
2024-09-11 16:45:10 +0300

    feat(map example): added disabled markers example

--- docs/pages/components/map/example.vue
+++ docs/pages/components/map/example.vue
@@ -1,25 +1,13 @@
 <template>
-	<ui-map
-		height="60vh" 
-		max-height="1000px" 
-		:zoom="8" 
-		:zoomMin="1" 
-		:zoomMax="18" 
-		:coord="[24.106389, 56.948889]"
-		:markers="markers" 
-		:disabledMarkers="disabledMarkers"
-		markerImage="/map-marker-violet.png"
-		markerActiveImage="/map-marker-green.png" 
-		markerDisabledImage="/map-marker-gray.png" 
-		@wheel="onMouseWheel" 
-		@initialized="onMapInitialized"
-	>
+	<ui-map height="60vh" max-height="1000px" :zoom="8" :zoomMin="1" :zoomMax="18" :coord="[24.106389, 56.948889]"
+		:markers="markers" :disabledMarkers="disabledMarkers" markerImage="/map-marker-violet.png"
+		markerActiveImage="/map-marker-green.png" markerDisabledImage="/map-marker-gray.png" @wheel="onMouseWheel"
+		@initialized="onMapInitialized">
 		<template #row="{ store }">
 			<ui-map-zoom :store="store">
 				<ui-map-device-zoom-switch :store="store">
 					<ui-tooltip v-if="!store.deviceZoom" v-model="showZoomTooltip" location="left"
-						textColor="var(--color-secondary)" bg="var(--color-bg)"
-					>
+						textColor="var(--color-secondary)" bg="var(--color-bg)">
 						"Click to enable mouse wheel or touch manipulations"
 					</ui-tooltip>
 				</ui-map-device-zoom-switch>
@@ -28,18 +16,14 @@
 			<ui-map-location _showDefaultSlot="store.selectedMarker">
 				<template #header>
 					<ui-map-location-list v-model="selectedCity" :options="cities" label="City" />
-					<ui-map-location-nearest :locations="markers" icon="ic ic-location-crosshairs" label="Find nearest department" />
+					<ui-map-location-nearest :locations="markers" icon="ic ic-location-crosshairs"
+						label="Find nearest department" />
 				</template>
 
 				<template #default>
 					<Transition name="slideup">
-						<div
-							v-if="store.selectedMarker" 
-							class="map-marker-info"
-							:data="store.selectedMarker"
-							map-layout
-							@click="store.setSelectedMarker(null)" 
-						>
+						<div v-if="store.selectedMarker" class="map-marker-info" :data="store.selectedMarker" map-layout
+							@click="store.setSelectedMarker(null)">
 							<p>Example output of marker info:</p>
 							<pre>{{ store.selectedMarker }}</pre>
 						</div>
@@ -52,83 +36,119 @@
 
 <script setup>
 const markers = [
-{
-        "id": "czxauig00",
-        "name": "Centrālā laboratorija",
-        "address": "Šarlotes iela 1B, Rīga, Rīgas 1. slimnīca",
-        "city": "riga",
-        "coord": [
-            24.12778506,
-            56.96157621
-        ],
-    },
-    {
-        "id": "cfla5iiz9",
-        "name": "A. Dombrovska filiāle",
-        "address": "A. Dombrovska iela 26, Rīga",
-        "city": "riga",
-        "coord": [
-            24.1081432,
-            57.0314181
-        ],
-    },
-    {
-        "id": "l7p95h4b8",
-        "name": "Āgenskalna filiāle",
-        "address": "Nometņu iela 60, Rīga, Āgenskalna doktorāts",
-        "city": "riga",
-        "coord": [
-            24.071860755676,
-            56.937342828933
-        ],
-    },
-    {
-        "id": "f1hvfze6b",
-        "name": "VCA Aura filiāle",
-        "address": "Nīcgales iela 5, Rīga, VCA Aura",
-        "city": "riga",
-        "coord": [
-            24.177260841125,
-            56.958010529539
-        ]
-    },
+	{
+		"id": "czxauig00",
+		"name": "Centrālā laboratorija",
+		"address": "Šarlotes iela 1B, Rīga, Rīgas 1. slimnīca",
+		"city": "riga",
+		"coord": [
+			24.12778506,
+			56.96157621
+		],
+	},
+	{
+		"id": "cfla5iiz9",
+		"name": "A. Dombrovska filiāle",
+		"address": "A. Dombrovska iela 26, Rīga",
+		"city": "riga",
+		"coord": [
+			24.1081432,
+			57.0314181
+		],
+	},
+	{
+		"id": "l7p95h4b8",
+		"name": "Āgenskalna filiāle",
+		"address": "Nometņu iela 60, Rīga, Āgenskalna doktorāts",
+		"city": "riga",
+		"coord": [
+			24.071860755676,
+			56.937342828933
+		],
+	},
+	{
+		"id": "f1hvfze6b",
+		"name": "VCA Aura filiāle",
+		"address": "Nīcgales iela 5, Rīga, VCA Aura",
+		"city": "riga",
+		"coord": [
+			24.177260841125,
+			56.958010529539
+		]
+	},
+	{
+		"id": "19",
+		"name": "Daugavpils filiāle (Viestura iela)",
+		"address": "Viestura iela 3, Daugavpils",
+		"city": "daugavpils",
+		"coord": [
+			26.52737,
+			55.8678524
+		],
+	},
+	{
+		"id": "20",
+		"name": "Daugavpils filiāle",
+		"address": "Lāčplēša iela 45, Daugavpils",
+		"city": "daugavpils",
+		"coord": [
+			26.52485390000004,
+			55.8717117
+		],
+	},
+	{
+		"id": "21",
+		"name": "Daugavpils filiāle Avene",
+		"address": "Aveņu iela 26, Daugavpils",
+		"city": "daugavpils",
+		"coord": [
+			26.5274284,
+			55.9012161
+		],
+	},
 ];
 
-const disabledMarkers = ref([]);
+const disabledMarkers = ref(['21']);
 
 const cities = ref([
 	{
-        "value": "all",
-        "name": "...",
-        "lat": 56.948889,
-        "lon": 24.106389,
+		"value": "all",
+		"name": "...",
+		"lat": 56.948889,
+		"lon": 24.106389,
 		"zoom": 8
-    },
-    {
-        "value": "riga",
-        "name": "Rīga",
-        "lat": 56.9496,
-        "lon": 24.1052,
-		"zoom": 12
-    },
+	},
 	{
-        "value": "adazi",
-        "name": "Ādaži",
-        "lat": 57.0707,
-        "lon": 24.3181
-    },
-    {
-        "value": "aizpute",
-        "name": "Aizpute",
-        "lat": 56.7227,
-        "lon": 21.6015
-    },
-    {
-        "value": "aluksne",
-        "name": "Alūksne",
-        "lat": 57.4215,
-        "lon": 27.0433
-    },
+		"value": "riga",
+		"name": "Rīga",
+		"lat": 56.9496,
+		"lon": 24.1052,
+		"zoom": 12
+	},
+	{
+		"value": "adazi",
+		"name": "Ādaži",
+		"lat": 57.0707,
+		"lon": 24.3181
+	},
+	{
+		"value": "aizpute",
+		"name": "Aizpute",
+		"lat": 56.7227,
+		"lon": 21.6015
+	},
+	{
+		"value": "aluksne",
+		"name": "Alūksne",
+		"lat": 57.4215,
+		"lon": 27.0433
+	},
+	{
+		"value": "daugavpils",
+		"name": "Daugavpils",
+		"lat": 55.8783,
+		"lon": 26.5437
+	},
 ]);
 
 const selectedCity = ref(null);
